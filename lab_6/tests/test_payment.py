8e1aaf5762ba0d904783f19b01e2b920d23cf012
PanDiablo <163389043+nikzavadskiy@users.noreply.github.com>
2025-12-18 21:54:03 +0400

    Update test_payment.py

--- lab_6/tests/test_payment.py
+++ lab_6/tests/test_payment.py
@@ -3,18 +3,20 @@ from unittest.mock import patch, Mock
 from src.payment import PaymentProcessor
 
 class TestPayment:
+    
     @patch('src.payment.requests', create=True)
     def test_convert_price_success(self, mock_requests):
         mock_response = Mock()
         mock_response.status_code = 200
         mock_response.json.return_value = {"rate": 0.5}
+        
         mock_requests.get.return_value = mock_response
 
         processor = PaymentProcessor("http://fake.api")
         result = processor.convert_price(100, "USD")
         
         assert result == 50.0
-        mock_get.assert_called_once()
+        mock_requests.get.assert_called_once()
 
     @patch('src.payment.requests', create=True)
     def test_convert_price_api_error(self, mock_requests):
